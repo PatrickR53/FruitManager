0553706bf7ef6e8bd59201ebd1626d423d47bcb0
PRuppé <patrick.ruppe.53@gmail.com>
2025-07-18 20:26:35 +0200

    ajout de la fonction de tresorerie

--- fruit_manager.py
+++ fruit_manager.py
@@ -1,6 +1,6 @@
 import json
 
-def ouvrir_inventaire(path="inventaire.json"):
+def ouvrir_inventaire(path="data/inventaire.json"):
     with open(path, 'r', encoding='utf-8') as fichier:
         inventaire = json.load(fichier)
     return inventaire
@@ -8,28 +8,57 @@ def ouvrir_inventaire(path="inventaire.json"):
 def ecrire_inventaire(inventaire,path="inventaire.json"):
     with open(path, 'w', encoding='utf-8') as fichier:
         json.dump(inventaire, fichier, ensure_ascii=False, indent=4)
+        
+def ouvrir_tresorerie(path="data/tresorerie.txt"):
+    with open(path, 'r', encoding='utf-8') as fichier:
+        tresorerie = json.load(fichier)
+    return tresorerie
+
+def ecrire_tresorerie(tresorerie,path="data/tresoserie.txt"):
+    with open(path, 'w', encoding='utf-8') as fichier:
+        json.dump(tresorerie, fichier, ensure_ascii=False, indent=4)
+        
+def afficher_tresorerie(tresorerie):
+    print(f"\n 💰 Trésorerie actuelle : {tresorerie:.2f} $")
+
 
 def afficher_inventaire(inventaire):
-    print("inventaire actuel de la plantation")
+    print("\n inventaire actuel de la plantation")
     for fruit, quantite in inventaire.items():
         print(f"- {fruit.capitalize()} : {quantite} unités")
+    print("\n Fin de l'inventaire")
         
 def recolter(inventaire, fruit, quantite):
     inventaire[fruit] = inventaire.get(fruit, 0) + quantite
     print(f"\n ✅ Récolté {quantite} {fruit} supplémentaire(s) !")
     
-def vendre(inventaire, fruit, quantite):
+def vendre(inventaire, fruit, quantite, tresorerie):
     if inventaire.get(fruit, 0) >= quantite:
         inventaire[fruit] -= quantite
+        tresorerie += 1 * quantite
         print(f"\n 💰 Vendu {quantite} {fruit} !")
+        return(inventaire, tresorerie)
     else:
         print(f"\n 🚨 quantite insuffisante de {fruit} pour vendre {quantite} unité(s) !")
 
 
 if __name__ == "__main__":
-    inventaire = ouvrir_inventaire
+    
+    inventaire = ouvrir_inventaire()
+    tresorerie = ouvrir_tresorerie()
+    
+    afficher_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
+    
     recolter(inventaire, "bananes", 10)
-    vendre(inventaire, "noix de coco", 15)
+    recolter(inventaire, "banoix de coco", 10)
+    
+    inventaire, tresorerie = vendre(inventaire, "noix de coco", 15, tresorerie)
+    
+    afficher_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
+    
+    ecrire_tresorerie(tresorerie)
     ecrire_inventaire(inventaire)
+    
+    
