c0e064733b79b3a032a4387605da0de3297deb62
PRuppé <patrick.ruppe.53@gmail.com>
2025-07-18 20:50:34 +0200

    ajout fonction prix du marché

--- fruit_manager.py
+++ fruit_manager.py
@@ -14,10 +14,15 @@ def ouvrir_tresorerie(path="data/tresorerie.txt"):
         tresorerie = json.load(fichier)
     return tresorerie
 
-def ecrire_tresorerie(tresorerie,path="data/tresoserie.txt"):
+def ecrire_tresorerie(tresorerie,path="data/tresorerie.txt"):
     with open(path, 'w', encoding='utf-8') as fichier:
         json.dump(tresorerie, fichier, ensure_ascii=False, indent=4)
         
+def ouvrir_prix(path="data/prix.json"):
+    with open(path, 'r', encoding='utf-8') as fichier:
+        prix = json.load(fichier)
+    return prix
+
 def afficher_tresorerie(tresorerie):
     print(f"\n 💰 Trésorerie actuelle : {tresorerie:.2f} $")
 
@@ -32,11 +37,11 @@ def recolter(inventaire, fruit, quantite):
     inventaire[fruit] = inventaire.get(fruit, 0) + quantite
     print(f"\n ✅ Récolté {quantite} {fruit} supplémentaire(s) !")
     
-def vendre(inventaire, fruit, quantite, tresorerie):
+def vendre(inventaire, fruit, quantite, tresorerie, prix):
     if inventaire.get(fruit, 0) >= quantite:
         inventaire[fruit] -= quantite
-        tresorerie += 1 * quantite
-        print(f"\n 💰 Vendu {quantite} {fruit} !")
+        tresorerie += prix.get(fruit, 0) * quantite
+        print(f"\n 💰 Vendu {quantite} {fruit} au prix {prix.get(fruit, 0)} $!")
         return(inventaire, tresorerie)
     else:
         print(f"\n 🚨 quantite insuffisante de {fruit} pour vendre {quantite} unité(s) !")
@@ -46,6 +51,7 @@ if __name__ == "__main__":
     
     inventaire = ouvrir_inventaire()
     tresorerie = ouvrir_tresorerie()
+    prix = ouvrir_prix()
     
     afficher_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
@@ -53,7 +59,7 @@ if __name__ == "__main__":
     recolter(inventaire, "bananes", 10)
     recolter(inventaire, "banoix de coco", 10)
     
-    inventaire, tresorerie = vendre(inventaire, "noix de coco", 15, tresorerie)
+    inventaire, tresorerie = vendre(inventaire, "noix de coco", 15, tresorerie, prix)
     
     afficher_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
