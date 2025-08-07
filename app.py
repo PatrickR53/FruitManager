faf6d166d3f1561a729406c1b9cd062c514561ad
PRuppé <patrick.ruppe.53@gmail.com>
2025-08-07 14:19:06 +0200

    Ajout du bouton vendre

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import streamlit as st
 from fruit_manager import *
 
-st.title("🍇 Fruit Manager")
+st.title("🍇 Dashboard de la plantation")
 
 inventaire = ouvrir_inventaire()
 prix = ouvrir_prix()
 tresorerie = ouvrir_tresorerie()
 
+with st.sidebar:
+    st.header(" 🛒 Vendre des Fruits")
+    fruit_vendre = st.selectbox("Choisir un Fruit", list(inventaire.keys()))
+    quantite_vendre = st.number_input("Qunatité à vendre",min_value=1, step=1)
+    
+    if st.button("Vendre"):
+        inventaire, tresorerie = vendre(inventaire, fruit_vendre, quantite_vendre, tresorerie, prix)
+
 st.header("💰 Trésorerie")
 st.metric(label="Montant disponible", value=f"{tresorerie:.2f} $")
 
-st.header(" Inventaire")
+st.header("📒 Inventaire")
 st.table(inventaire)
